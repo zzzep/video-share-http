37534aa4d838db28925e87326d9963ab1a34d424
Giuseppe Dezute Fechio <zzzep@hotmail.com>
2020-04-26 00:23:20 -0300

    add more examples and more type of files

--- main.go
+++ main.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type Video struct {
+	fullPath string
+	name     string
+	fileType string
+}
+
+var videoTypes = map[string]string{
+	"default": "video/webm",
+	".mp4": "video/mp4",
+	".mkv": "video/webm",
+	".m4v": "video/webm",
+	".ogg": "video/ogg",
+}
 
 func main() {
 	http.HandleFunc("/home", videoPage)
-	http.HandleFunc("/",fileH)
+	http.HandleFunc("/", fileH)
 	http.ListenAndServe(":8080", nil)
 }
 
 func fileH(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "videos/example.mp4")
+	f := r.URL.Path
+	f = string(bytes.TrimLeft([]byte(f), "/"))
+	f = string(bytes.TrimLeft([]byte(f), "\\"))
+	http.ServeFile(w, r, f)
 }
 
 func videoPage(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +41,14 @@ func videoPage(w http.ResponseWriter, r *http.Request) {
 
 	videos := getVideos(p)
 
-	_, _ = fmt.Fprintf(w, "<html><head></head><body><center>")
+	_, _ = fmt.Fprintf(w, "<!DOCTYPE html><html>")
+	_, _ = fmt.Fprintf(w, "<head></head>")
+	_, _ = fmt.Fprintf(w, "<body><center>")
 	for _, v := range videos {
-		_, _ = fmt.Fprintln(w, v)
-		_, _ = fmt.Fprintf(w, "<br><embed src=\"%s\" allowfullscreen=\"true\" width=\"300\">", v)
+		_, _ = fmt.Fprintln(w, v.name)
+		_, _ = fmt.Fprintln(w, "<br>")
+		//_, _ = fmt.Fprintf(w, "<embed src=\"%s\" width=\"300\" autostart=\"0\">", v.fullPath)
+		_, _ = fmt.Fprintf(w, "<video width=\"300px\" controls><source src=\"%s\" type=\"%s\"></video>", v.fullPath, v.fileType)
 		_, _ = fmt.Fprintln(w, "<br>")
 	}
 	_, _ = fmt.Fprintf(w, "</center></body></html>")
@@ -48,17 +71,32 @@ func getPath(r *http.Request) string {
 	return d
 }
 
-func getVideos(path string) []string {
+func getVideos(path string) []Video {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	var videos []string
+	var videos []Video
 
 	for _, f := range files {
-		videos = append(videos, path+"/"+f.Name())
+		var v Video
+		v.name = f.Name()
+		v.fullPath = path + "/" + v.name
+		v.fileType = getVideoType(v)
+		videos = append(videos, v)
 	}
 
 	return videos
+}
+
+func getVideoType(v Video) string {
+	ext := filepath.Ext(v.name)
+
+	t, exists := videoTypes[ext]
+	if exists == true {
+		return t
+	}
+
+	return videoTypes["default"]
 }
